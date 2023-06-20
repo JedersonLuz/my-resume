4dfce3ac5dd4ba82c614810723ba2dde0b6fce42
JedersonLuz <jedersonalpha@gmail.com>
2023-06-19 22:18:41 -0300

    My resume v1.0.0

---
+++ src/data/info.ts
@@ -0,0 +1,96 @@
+export const info = {
+  baseUrl: "https://jedersonluz.dev",
+  name: "Jederson Luz",
+  jobDescription: "Machine Learning Engineer",
+  about: `I'm a Software and Machine Learning Engineer undergraduate with a 
+  Bachelor's in Information Systems at the Federal University of Piauí - UFPI and a
+  Master's student in Electrical Engineering at the Federal University of Piauí - UFPI.
+  I'm currently working on my projects building solutions with web and mobile apps, and machine learning.`,
+
+  experience: [
+    {
+      name: "Docket Brasil (Remote)",
+      location: "São Paulo, SP, Brazil",
+      startDate: "May 2021",
+      endDate: "Nov 2022",
+      description: [
+        `I work helping in the process of continuous improvement of one of the company's artificial intelligence products, 
+        R.E.A. (Real Estate Analysis), where I work developing solutions with computer vision and natural language processing,
+        developing artificial intelligence models. In addition, I assist in the process of developing and improving the 
+        team's dataset annotation tool.`,
+      ],
+    },
+
+    {
+      name: "JurisfAI (Remote)",
+      location: "Santos, SP, Brazil",
+      startDate: "Jul 2020",
+      endDate: "Feb 2021",
+      description: [
+        `Intern at Lawtech JurisfAI, working in Natural Language Processing, Machine Learning, 
+        API development and Web Scraping, contributing to the development of automation solutions
+        for the startup's legal processes.`,
+      ],
+    },
+
+    {
+      name: "Federal University of Piauí - UFPI",
+      location: "Picos, PI, Brazil",
+      startDate: "May 2018",
+      endDate: "May 2021",
+      description: [
+        `Scientific Initiation Student at the Federal University of Piauí (UFPI). 
+        I have developed scientific works focused on the areas of Vision and Computational
+        Intelligence applied in the context of audio signal processing in urban scenarios.`,
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Federal University of Piauí - UFPI",
+      location: "Teresina, PI, Brazil",
+      startDate: "2021",
+      endDate: "Present",
+      description: [
+        "Master's student in Electrical Engineering, with an emphasis on Computational Intelligence.",
+      ],
+    },
+    {
+      name: "Federal University of Piauí - UFPI",
+      location: "Picos, PI, Brazil",
+      startDate: "2017",
+      endDate: "2021",
+      description: [
+        "Bachelor's in Information Systems, with an emphasis on Software Engineering.",
+      ],
+    },
+    {
+      name: "Federal Institute of Piauí - IFPI",
+      location: "Picos, PI, Brazil",
+      startDate: "2012",
+      endDate: "2016",
+      description: [
+        "High School with Technical course in Computer Science, with an emphasis on Software Development.",
+      ],
+    },
+  ],
+
+  socialMedia: {
+    // facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Jederson_Luz",
+    github: "https://github.com/JedersonLuz",
+    email: "mailto:jedersonalpha@gmail.com",
+    linkedin: "https://www.linkedin.com/in/Jederson_Luz/",
+  },
+
+  projects: [
+    // {
+    //   title: "Astro Portfolio",
+    //   isFeatured: true,
+    //   thumbnail: "/assets/images/astro-portfolio.png",
+    //   githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+    //   liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    // },
+  ],
+};
